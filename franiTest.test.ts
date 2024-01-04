83d0f8d1adfff084c441f720b9544c9222440ba3
Franciska Szente <frani.szente@gmail.com>
2024-01-03 18:54:37 -0800

    franitest push

--- franiTest.test.ts
+++ franiTest.test.ts
@@ -2,7 +2,7 @@ import { Frani } from "./franiPage";
 const page = new Frani(); 
 
 
-test("Click on the pasuse and play button", async () => {
+test("Testing pause and play button", async () => {
     await page.navigate();
     await page.driver.manage().window().maximize()
     await page.driver.sleep(2000);
@@ -14,19 +14,43 @@ test("Click on the pasuse and play button", async () => {
 
 })
 
-test("Click on order now", async () => {
+test("Adding and item to cart", async () => {
     await page.navigate();
     await page.driver.sleep(2000);
     await page.click(page.orderNow);
-    await page.driver.sleep(3000);
+    await page.driver.sleep(2000);
     await page.click(page.milkshakes);
-    await page.driver.sleep(3000);
+    await page.driver.sleep(2000);
     await page.click(page.houseMadeMilkshakes);
-    await page.driver.sleep(3000);
+    await page.driver.sleep(2000);
     await page.click(page.vanilla);
     await page.driver.sleep(3000);
     await page.click(page.addToBag);
-    await page.driver.sleep(4000);
+    await page.driver.sleep(3000);
+
+})
+
+test("Testing search bar", async () => {
+    await page.navigate();
+    await page.driver.sleep(2000);
+    await page.click(page.findYourIhop);
+    await page.setInput(page.zipcode, '94109');
+    await page.driver.sleep(2000);
+    await page.click(page.search);
+    await page.driver.sleep(2500);
+
+})
+
+test("Creating an account", async () => {
+    await page.navigate();
+    await page.driver.sleep(2000);
+    await page.click(page.join);
+    await page.setInput(page.email, 'john103@gmail.com');
+    await page.driver.sleep(2000);
+    await page.setInput(page.password, 'Freshapple1010!');
+    await page.driver.sleep(2000);
+    await page.click(page.continue);
     await page.driver.quit()
+
 
 })
