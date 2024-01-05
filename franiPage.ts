79490036dcac8388e89efd31aa861def57bf68f0
Franciska Szente <frani.szente@gmail.com>
2024-01-04 18:08:27 -0800

    franiTest push

--- franiPage.ts
+++ franiPage.ts
@@ -8,6 +8,7 @@ export class Frani extends BasePage {
     milkshakes: By = By.xpath('(//div[@class="h4 card-heading"])[23]');
     houseMadeMilkshakes: By = By.xpath('(//div[@class="subcategory-description"])[1]');
     vanilla: By = By.xpath('(//div[@class="category-description"])[1]');
+    xBtn: By = By.xpath('(//button[@class="counter-plus btn"])[1]')
     addToBag: By = By.xpath('(//button[@*])[6]');
     findYourIhop: By = By.xpath('(//a[@class="home-features__link"])[2]');
     locationSearch: By = By.xpath('(//input[@class="ui-autocomplete-input"])[1]');
@@ -16,6 +17,15 @@ export class Frani extends BasePage {
     email: By = By.xpath('(//input[@class="input c1a815477 c5c5d6a3a"])[1]');
     password: By = By.xpath('(//input[@class="input c1a815477 c61ad6f21"])[1]');
     continue: By = By.xpath('(//button[@*])[2]');
+    firstName: By = By.xpath('(//input[@*])[22]');
+    lastName: By = By.xpath('(//input[@*])[23]');
+    zipCode: By = By.xpath('(//input[@*])[24]');
+    location: By = By.xpath('(//select[@*])[1]');
+    mobile: By = By.xpath('(//input[@*])[25]');
+    checkMark: By = By.xpath('(//div[@*])[109]');
+    signUp: By = By.xpath('(//button[@class="btn btn-lg w-100 continue-okta-login sign-up"])[1]');
+
+
 
 
     constructor(){
--- franiTest.test.ts
+++ franiTest.test.ts
@@ -1,17 +1,25 @@
 import { Frani } from "./franiPage";
 const page = new Frani(); 
-let str = '94109';
 
-
-test("Testing pause and play button", async () => {
+test("Creating an account", async () => {
     await page.navigate();
     await page.driver.manage().window().maximize()
+    await page.click(page.join);
+    await page.setInput(page.email, 'maxim85@gmail.com');
     await page.driver.sleep(2000);
-    await page.click(page.pauseVideo);
-    await page.driver.sleep(1000);
-    await page.click(page.playVideo);
-    await page.driver.sleep(1000);
-    await page.click(page.playVideo);
+    await page.setInput(page.password, 'Freshapple1010!');
+    await page.driver.sleep(2000);
+    await page.click(page.continue);
+    await page.setInput(page.firstName, 'Maxim');
+    await page.setInput(page.lastName, 'Smith');
+    await page.setInput(page.zipCode, '94105');
+    await page.click(page.location);
+    await page.setInput(page.mobile, '7072584136');
+    await page.click(page.checkMark);
+    await page.driver.sleep(2000);
+    await page.click(page.signUp);
+    await page.driver.sleep(3000);
+
 
 })
 
@@ -25,7 +33,9 @@ test("Adding and item to cart", async () => {
     await page.click(page.houseMadeMilkshakes);
     await page.driver.sleep(2000);
     await page.click(page.vanilla);
-    await page.driver.sleep(3000);
+    await page.driver.sleep(2000);
+    await page.click(page.xBtn);
+    await page.driver.sleep(2000);
     await page.click(page.addToBag);
     await page.driver.sleep(3000);
 
@@ -38,22 +48,20 @@ test("Testing location search", async () => {
     await page.setInput(page.locationSearch, '94109');
     await page.click(page.searchBtn);
     await page.driver.sleep(2000);
-    console.log(str.replace(/\d{Chicago}/,''));
+    await page.setInput(page.locationSearch, 'Chicago');
     await page.click(page.searchBtn);
     await page.driver.sleep(2500);
 
 })
 
-test("Creating an account", async () => {
+test("Testing pause and play button", async () => {
     await page.navigate();
     await page.driver.sleep(2000);
-    await page.click(page.join);
-    await page.setInput(page.email, 'jack103@gmail.com');
-    await page.driver.sleep(2000);
-    await page.setInput(page.password, 'Freshapple1010!');
-    await page.driver.sleep(2000);
-    await page.click(page.continue);
+    await page.click(page.pauseVideo);
+    await page.driver.sleep(1000);
+    await page.click(page.playVideo);
+    await page.driver.sleep(1000);
+    await page.click(page.playVideo);
     await page.driver.quit()
-
 
 })
